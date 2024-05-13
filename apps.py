f09c0b49e981475c5ecb41a1e13916a5b333a290
Daniel Lara <mat.daniel.lara@outlook.com>
2024-05-13 15:52:14 -0500

    Actualizacion nombre modulo

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SegaulasConfig(AppConfig):
+class ReportesCEVConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'SegAulas'
+    name = 'ReportesCEV'
