1506e60e85c51331b3fda2542371fab6df66ec7d
Daniel Lara <mat.daniel.lara@outlook.com>
2024-05-21 16:46:23 -0500

    feat: Se actualiza a un procesamiento asincrónico con notificación por correo. ✨

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
+from django.template.loader import render_to_string
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render
 from .forms import FormularioSeguimientoAulas
@@ -11,6 +12,8 @@ import os
 from django.contrib import messages
 from django.conf import settings
 import sys
+from django.core.mail import EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
 
 def is_member_puce(user):
     return user.groups.filter(name="PUCE_Basico").exists()
@@ -47,7 +50,7 @@ def elaborar_reportes(identificador_proceso, nombre_coordinador):
             identificador_proceso,
         )
 
-        # Movemos el archivo comprimido a la carpeta uploads/comprimidos_cev para su descarga 
+        # Movemos el archivo comprimido a la carpeta uploads/comprimidos_cev para su descarga
         ruta_comprimido = os.path.join(
             MEDIA_ROOT, "comprimidos_cev", f"Seguimiento-{identificador_proceso}.zip"
         )
@@ -57,10 +60,7 @@ def elaborar_reportes(identificador_proceso, nombre_coordinador):
         )
 
         # Movemos el archivo comprimido a la carpeta uploads/comprimidos_cev para su descarga
-        os.rename(
-            ruta_generado,
-            ruta_comprimido
-        )
+        os.rename(ruta_generado, ruta_comprimido)
 
         # Guardamos la ruta en el modelo con el identificador del proceso
         ModeloSeguimientoAulas.objects.filter(IdProceso=identificador_proceso).update(
@@ -84,14 +84,14 @@ def elaborar_reportes(identificador_proceso, nombre_coordinador):
         mensaje_error = str(sys.exc_info()[1])
 
         respuesta = {
-        "status": "error",
-        "type" : "FileNotFoundError",
-        "message": mensaje_error,
+            "status": "error",
+            "type": "FileNotFoundError",
+            "message": mensaje_error,
         }
 
         # Volvemos al directorio de trabajo original
         os.chdir(settings.BASE_DIR)
-        
+
         return respuesta
 
     except ValueError:
@@ -99,9 +99,9 @@ def elaborar_reportes(identificador_proceso, nombre_coordinador):
         mensaje_error = str(sys.exc_info()[1])
 
         respuesta = {
-        "status": "error",
-        "type" : "ValueError",
-        "message": mensaje_error,
+            "status": "error",
+            "type": "ValueError",
+            "message": mensaje_error,
         }
 
         # Volvemos al directorio de trabajo original
@@ -115,15 +115,16 @@ def elaborar_reportes(identificador_proceso, nombre_coordinador):
         mensaje_error = str(sys.exc_info()[1])
 
         respuesta = {
-        "status": "error",
-        "type" : "Exception",
-        "message": mensaje_error,
+            "status": "error",
+            "type": "Exception",
+            "message": mensaje_error,
         }
 
         # Volvemos al directorio de trabajo original
         os.chdir(settings.BASE_DIR)
 
         return respuesta
+
 
 @user_passes_test(is_member_puce)
 @login_required
@@ -144,7 +145,36 @@ def GenerarReportesSeguimiento(request):
             # Obtenemos el nombre del coordinador
             nombre_coordinador = form.cleaned_data["NombreCoordinador"]
 
-            json_respuesta = elaborar_reportes(identificador_proceso, nombre_coordinador)
+            json_respuesta = elaborar_reportes(
+                identificador_proceso, nombre_coordinador
+            )
+
+            if json_respuesta["status"] == "success":
+        
+                mensaje = render_to_string('Extras/PlanitllaEnvioSeguimiento.html', {
+                    'user': request.user,
+                    'domain' : get_current_site(request).domain,
+                    'codigo': identificador_proceso,
+                })
+                
+                EmailMessage(
+                    'Reporte de Seguimiento de Aulas',
+                    mensaje,
+                    to=[request.user.email]
+                ).send()
+            else:
+
+                mensaje = render_to_string('Extras/PlanitllaErrorEnvioSeguimiento.html', {
+                    'user': request.user,
+                    'type' : json_respuesta["type"],
+                    'message' : json_respuesta["message"],
+                })
+                
+                EmailMessage(
+                    'Error: Reporte de Seguimiento de Aulas',
+                    mensaje,
+                    to=[request.user.email]
+                ).send()
 
             return JsonResponse(json_respuesta, safe=False)
 
@@ -186,6 +216,8 @@ def DescargarReporteSeguimientoAulas(request, identificador_proceso):
 
         # Generamos la respuesta
         response = HttpResponse(contenido, content_type="application/zip")
-        response["Content-Disposition"] = f"attachment; filename=Seguimiento{identificador_proceso}.zip"
+        response["Content-Disposition"] = (
+            f"attachment; filename=Seguimiento{identificador_proceso}.zip"
+        )
 
         return response
